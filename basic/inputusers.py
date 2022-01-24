8eee56beb11079feca2f85f33d5a9802f52709b0
Shruti Mishra <shrutidmishra2002@gmail.com>
2022-01-24 22:37:21 +0530

    added:python programs to basic folder

---
+++ basic/inputusers.py
@@ -0,0 +1,9 @@
+print("enter 2 nos to be added:")
+#storing input from users
+num1=input("enter first no:")
+num2=input("enter second no:")
+#write int or any datatype for adding otherwise 
+#you will get 3 + 3 , 33
+sum= int(num1) + int(num2)
+print("sum is:",sum)
+
---
+++ basic/variable.py
@@ -0,0 +1,6 @@
+print("addition of 2 nos ")
+x=10
+y=8
+sum=x+y
+print(sum)
+print("sum is:",sum)
